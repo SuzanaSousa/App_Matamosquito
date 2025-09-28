e6fb606ef6b487639c0a095c767797259a66f33c
Suzana  Sousa <106107598+SuzanaSousa@users.noreply.github.com>
2025-09-27 22:56:31 -0300

    Add files via upload

--- jogo.js
+++ jogo.js
@@ -1,55 +1,94 @@
  var altura =  0
  var largura = 0
  var vidas = 1
+ var tempo = 15
 
-function ajustaTamanhoPalcoJogo()
-{
+ var criaMosquitoTempo = 1500
+ 
+ var nivel = window.location.search
+
+ alert(nivel.replace('?', ''))
+
+ if(nivel === '?normal'){
+  //1500
+    }else if(nivel  === '?dificil'){
+        criaMosquitoTempo = 1000
+    //1000
+    }else if(nivel === '?chucknorris'){
+        criaMosquitoTempo = 750
+    //750
+    }
+
+function ajustaTamanhoPalcoJogo(){
     var altura = window.innerHeight
     var largura = window.innerWidth
 
-    console.log( largura, altura)
+    console.log(largura, altura)
 }
 ajustaTamanhoPalcoJogo()
 
+var cronometro = setInterval(function() {
+    tempo -=1
+
+    if( tempo < 0){
+        clearInterval(cronometro)
+        clearInterval(criaMosca)
+        window.location.href = "vitoria.html?" + nivel
+
+    }else{
+        document.getElementById('cronometro').innerHTML = tempo
+    }
+    
+}, 1000);
+// --- FUNÇÃO DE POSIÇÃO RANDOMICA ---
 function posicaoRandomica() {
 
-        //remover o mosquito anterior (caso exista)
-        if( document.getElementById('mosquito')){
-            document.getElementById('mosquito').remove()
-            if(vidas > 3){
-                window.location.href = 'fim_de_jogo.html'
+    //remover o mosquito anterior (caso exista)
+    if( document.getElementById('mosquito')){
+        document.getElementById('mosquito').remove()
+
+         // Lógica de Vidas
+        if(vidas > 3){
+                window.location.href = "fim_de_jogo.html"
             }else{
-                document.getElementById('v' + vidas).src = "img/coracao_vazio.png"
+              document.getElementById('v' + vidas).src = "img/coracao_vazio.png"
 
-                vidas ++
-            }
-
+              vidas ++
         }
-        
+
+    }
+         // Calcula posição com base nas dimensões globais (agora atualizadas)
         var posicaoX = Math.floor(Math.random() * largura) - 90
         var posicaoY = Math.floor(Math.random() * altura) - 90
 
         posicaoX = posicaoX < 0 ? 0 : posicaoX
         posicaoY = posicaoY < 0 ? 0 : posicaoY
 
-        console.log(posicaoX, posicaoY)
+       
+          // console.log(posicaoX, posicaoY) // Útil para debug   
 
-        //criar elemento html
+        //criar elemento html (o mosquito)
         var mosquito = document.createElement('img')
-        mosquito.src = './img/mosquito.png'
+        mosquito.src = './img/mosquito.png' // Corrigi o caminho para 'img/mosquito.png'
+        document.body.appendChild(mosquito)
         mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio() //valor retonado pelo chamado da função sera atribuido como a classe do html
         mosquito.style.left = posicaoX + 'px'
         mosquito.style.top = posicaoY + 'px'
         mosquito.style.position = 'absolute'
         mosquito.id = 'mosquito'
+
+         // Adiciona a função de clique para remover o mosquito
         mosquito.onclick = function()  {
            this.remove()//this faz o proprio elemento da função
         }
 
         document.body.appendChild(mosquito)
-        
+       /*  tamanhoAleatorio()
+         */
 }
+// --- FUNÇÕES AUXILIARES ---
 function tamanhoAleatorio() {
+    
     var classe = Math.floor(Math.random() * 3)
 
     switch(classe) {
@@ -63,7 +102,7 @@ function tamanhoAleatorio() {
 }
 function ladoAleatorio() {
     var classe = Math.floor(Math.random() * 2)
-    switch(classe)  {
+    switch(classe) {
         case 0:
             return 'ladoA'
         case 1:
